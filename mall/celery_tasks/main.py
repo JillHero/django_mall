ca672dfb97da77b645996e89f949111ae0afde05
JillHero <jillhero@hotamil.com>
2020-02-09 15:58:44 +0800

    "检查用户数量以及手机号"

--- mall/celery_tasks/main.py
+++ mall/celery_tasks/main.py
@@ -1,11 +1,12 @@
 from celery import Celery
 import os
 
-if not os.getenv("DJANGO_SETTINGS_MODULE"):
+if not os.getenv("DJANGO_SETTINGS_MODULE"):  # 获取全局变量DJANGO_SETTINGS_MODULE
     os.environ["DJANGO_SETTINGS_MODULE"] = 'mall.settings.dev'
 celery_app = Celery("mall")
 celery_app.config_from_object("celery_tasks.config")
 
-
 # 导入任务
 celery_app.autodiscover_tasks(['celery_tasks.sms'])
+
+
--- mall/mall/apps/users/views.py
+++ mall/mall/apps/users/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#  usernames/(?P<username>\w{5,20})/count/
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from mall.apps.users.models import User
+
+
+class UsernameCountView(APIView):
+    def get(self, request, username):
+        count = User.objects.filter(username=username).count()
+        data = {
+            "username": username,
+            "count": count
+        }
+        return Response(data, status=status.HTTP_200_OK)
+
+
+class MobileCountView(APIView):
+    def get(self, request, mobile):
+        count = User.objects.filter(mobile=mobile).count()
+        data = {
+            "mobile": mobile,
+            "count": count
+        }
+        return Response(data, status=status.HTTP_200_OK)
